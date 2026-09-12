d35b51c7631c5d58af8dfeadf3c93c1b828d3c18
nexapk-android <v9134502@gmail.com>
2026-09-12 21:45:06 +0530

    Update page.tsx

--- app/dmca/page.tsx
+++ app/dmca/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-const siteUrl = "https://enmodapk.vercel.app";
+const siteUrl = "https://genmod.in";
 
 export const metadata: Metadata = {
   title: "DMCA & Copyright Policy",
